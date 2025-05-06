9597562d287c59ab48ebdcd77963f8c0e4586db1
santiago456ls-l <s71000592@gmail.com>
2025-05-05 22:55:45 -0300

    Versión 2 del proyecto

---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,35 @@
+import CalorieDisplay from "./CalorieDisplay"
+import { useActivity } from "../hooks/useActivity"
+
+
+export default function CalorieTracker() {
+
+    const {caloriesConsumed, caloriesBurned, netCalories} = useActivity()
+    //contadores
+
+    return (
+        <>
+            <h2 className="text-4xl font-black text-white text-center">Resumen de Calorias</h2>
+            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+                
+                <CalorieDisplay
+                    calories={caloriesConsumed}
+                    text='Consumidas'
+                />
+                
+                <CalorieDisplay
+                    calories={caloriesBurned}
+                    text='Quemadas'
+                />
+
+                <CalorieDisplay
+                    calories={netCalories}
+                    text='Diferencia'
+                />
+
+            </div>
+            
+        </>
+
+    )
+}
